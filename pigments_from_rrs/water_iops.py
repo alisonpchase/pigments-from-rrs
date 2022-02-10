aead1ff1d90c0c2a28d7c4d0f74cd9d3f328d2dd
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-02-10 12:44:13 -0800

    work on iops funcs + tests

--- pigments_from_rrs/water_iops.py
+++ pigments_from_rrs/water_iops.py
@@ -1,12 +1,20 @@
 """TODO: Add link to Matlab source.
 """
+import numpy as np
+
+from typing import Tuple, Union
+
 # define input arrays
 #WAVEL_WATER = importlib.resources('water_iops', 'data/water_abs.csv')
 
 # Matlab function definition line:
 #    `function [a_sw,bb_sw] = get_water_iops(wave,T,S)`
 
-def RInw(lambda_, Tc, S):
+def RInw(
+    lambda_: Union[int, float, np.ndarray],
+    Tc: Union[int, float],
+    S: Union[int, float],
+) -> Tuple[Union[float, np.ndarray], Union[float, np.ndarray]]:
     """Refractive index of air is from Ciddor (1996,Applied Optics)
     Refractive index of seawater is from Quan and Fry (1994, Applied Optics)
     """
@@ -34,25 +42,10 @@ def RInw(lambda_, Tc, S):
     return nsw, dnswds
 
 
-def BetaT(Tc, S):
-    """
-    function IsoComp = BetaT(Tc, S)
-    % pure water secant bulk Millero (1980, Deep-sea Research)
-    kw = 19652.21+148.4206*Tc-2.327105*Tc.^2+1.360477e-2*Tc.^3-5.155288e-5*Tc.^4;
-    Btw_cal = 1./kw;
-
-    % isothermal compressibility from Kell sound measurement in pure water
-    % Btw = (50.88630+0.717582*Tc+0.7819867e-3*Tc.^2+31.62214e-6*Tc.^3-0.1323594e-6*Tc.^4+0.634575e-9*Tc.^5)./(1+21.65928e-3*Tc)*1e-6;
-
-    % seawater secant bulk
-    a0 = 54.6746-0.603459*Tc+1.09987e-2*Tc.^2-6.167e-5*Tc.^3;
-    b0 = 7.944e-2+1.6483e-2*Tc-5.3009e-4*Tc.^2;
-
-    Ks =kw + a0.*S + b0.*S.^1.5;
-
-    % calculate seawater isothermal compressibility from the secant bulk
-    IsoComp = 1./Ks*1e-5; % unit is pa
-    end
+def BetaT(Tc: Union[int, float], S: Union[int, float]) -> float:
+    """Pure water secant bulk Millero (1980, Deep-sea Research).
+    Isothermal compressibility from Kell sound measurement in pure water.
+    Calculate seawater isothermal compressibility from the secant bulk.
     """
     kw = (
         19652.21 + 148.4206 * Tc - 2.327105 * Tc ** 2 + 1.360477e-2 
@@ -63,9 +56,71 @@ def BetaT(Tc, S):
     b0 = 7.944e-2 + 1.6483e-2 * Tc - 5.3009e-4 * Tc ** 2
 
     Ks = kw + a0 * S + b0 * S ** 1.5
-    IsoComp = 1 / Ks * 1e0 - 5
+    IsoComp = 1 / Ks * 1e-5
 
     return IsoComp
+
+
+def rho_sw(Tc: Union[int, float], S: Union[int, float]) -> float:
+    """Density of water and seawater, unit is Kg/m^3, from UNESCO,38,1981.
+    TODO: Compare to GSW Oceanographic Toolbox code (or other updated) density equations.
+    """
+    a0 = 8.24493e-1
+    a1 = -4.0899e-3
+    a2 = 7.6438e-5
+    a3 = -8.2467e-7
+    a4 = 5.3875e-9
+    a5 = -5.72466e-3
+    a6 = 1.0227e-4
+    a7 = -1.6546e-6
+    a8 = 4.8314e-4
+    b0 = 999.842594
+    b1 = 6.793952e-2
+    b2 = -9.09529e-3
+    b3 = 1.001685e-4
+    b4 = -1.120083e-6
+    b5 = 6.536332e-9
+
+    # density for pure water
+    density_w = b0 + b1 * Tc + b2 * Tc ** 2 + b3 * Tc ** 3 + b4 * Tc ** 4 + b5 * Tc ** 5
+    # density for pure seawater
+    density_sw = (
+        density_w + ((a0 + a1 * Tc + a2 * Tc ** 2 + a3 * Tc ** 3 + a4 * Tc ** 4) * S
+        + (a5 + a6 * Tc + a7 * Tc ** 2) * S ** 1.5 + a8 * S ** 2)
+    )
+    return density_sw
+
+
+def dlnasw_ds(Tc: Union[int, float], S: Union[int, float]) -> float:
+    """Water activity data of seawater is from Millero and Leung (1976,American
+    Journal of Science,276,1035-1077). Table 19 was reproduced using
+    Eqs.(14,22,23,88,107) then were fitted to polynominal equation.
+    dlnawds is partial derivative of natural logarithm of water activity
+    w.r.t.salinity.
+
+    lnaw =  (-1.64555e-6-1.34779e-7*Tc+1.85392e-9*Tc.^2-1.40702e-11*Tc.^3)+......
+            (-5.58651e-4+2.40452e-7*Tc-3.12165e-9*Tc.^2+2.40808e-11*Tc.^3).*S+......
+            (1.79613e-5-9.9422e-8*Tc+2.08919e-9*Tc.^2-1.39872e-11*Tc.^3).*S.^1.5+......
+            (-2.31065e-6-1.37674e-9*Tc-1.93316e-11*Tc.^2).*S.^2;
+    
+    density derivative of refractive index from PMH model
+    """
+
+    dlnawds = (
+        (-5.58651e-4 + 2.40452e-7 * Tc - 3.12165e-9 * Tc ** 2 + 2.40808e-11 * Tc ** 3)
+        + 1.5 * (1.79613e-5 - 9.9422e-8 * Tc + 2.08919e-9 * Tc ** 2 - 1.39872e-11 * Tc ** 3) * S ** 0.5
+        + 2 * (-2.31065e-6 - 1.37674e-9 * Tc - 1.93316e-11 * Tc ** 2) * S
+    )
+    return dlnawds
+
+
+def PMH(n_wat: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+    n_wat2 = n_wat ** 2
+    n_density_derivative = (
+        (n_wat2 - 1) * (1 + 2 / 3 * (n_wat2 + 2)
+        * (n_wat / 3 - 1 / 3 / n_wat) ** 2)
+    )
+    return n_density_derivative
 
 
 def get_water_iops(wavel, T, S):
@@ -87,29 +142,89 @@ def get_water_iops(wavel, T, S):
     bb_sw: (array)
         Backscattering of seawater at each wavelenght in wavel for the specified temperature and salinity values.
     """
+    b = BetaT(4)
     pass   # this function doesn't do anything yet, so we're just going to `pass` for now
 
 
-# Matlab function definition line:
-#   `function [betasw124, bsw, beta90sw, theta] = betasw124_ZHH2009(lambda, S, Tc, delta)`
+def betasw124_ZHH2009(lambda_, S, Tc, delta=0.039):
+    """Scattering by pure seawater: Effect of salinity
+    Xiaodong Zhang, Lianbo Hu, and Ming-Xia He, Optics Express, 2009, accepted
+    lambda (nm): wavelength
+    Tc: temperauter in degree Celsius, must be a scalar
+    S: salinity, must be scalar
+    delta: depolarization ratio, if not provided, default = 0.039 will be used (from Farinato and Roswell (1976))
+    betasw: volume scattering at angles defined by theta. Its size is [x y],
+    where x is the number of angles (x = length(theta)) and y is the number
+    of wavelengths in lambda (y = length(lambda))
+    beta90sw: volume scattering at 90 degree. Its size is [1 y]
+    bw: total scattering coefficient. Its size is [1 y]
+    for backscattering coefficients, divide total scattering by 2
+    Xiaodong Zhang, March 10, 2009
 
-def betasw124_ZHH2009(wavel, S, Tc, delta):
+    MODIFIED on 17/05/2011 to be able to process bbp profiles with coincident T and sal profiles
+    MODIFIED on 05 Apr 2013 to use 124 degs instead of 117 degs
     """
-    
-    Parameters
-    ----------
-    wavel: (array)
-        Wavelenghts (nm) 
-    S: (float or int)
-        Salinity of water
-    Tc: (float or int)
-        Temperature (degC) of the water
-    delta: (float)
-        Depolarization ratio, default = 0.039
 
-    Returns
-    -------
+    # TODO: Support S and Tc as column vectors
+    for param in [S, Tc]:
+        if type(param) not in (int, float) or isinstance(param, np.ndarray):
+            raise NotImplementedError("S and Tc must be scalar.")
+
+    # values of the constants
+    Na = 6.0221417930e23  # Avogadro's constant
+    Kbz = 1.3806503e-23  # Boltzmann constant
+    Tk = Tc + 273.15  # Absolute temperature
+    M0 = 18e-3  # Molecular weight of water in kg/mol
+
+    theta = np.linspace(0.0, 180.0, 18_001)
+
+    rad = theta * np.pi/180  # angle in radians as a 1-d array
+
+    # nsw: absolute refractive index of seawater
+    # dnds: partial derivative of seawater refractive index w.r.t. salinity
+    nsw, dnds = RInw(lambda_, Tc, S)
+
+    # isothermal compressibility is from Lepple & Millero (1971,Deep Sea-Research), pages 10-11
+    # The error ~ +/-0.004e-6 bar^-1
+    IsoComp = BetaT(Tc, S)
+
+    # density of water and seawater,unit is Kg/m^3, from UNESCO,38,1981
+    density_sw = rho_sw(Tc, S)
+
+    # water activity data of seawater is from Millero and Leung (1976,American
+    # Journal of Science,276,1035-1077). Table 19 was reproduced using
+    # Eq.(14,22,23,88,107) then were fitted to polynominal equation.
+    # dlnawds is partial derivative of natural logarithm of water activity
+    # w.r.t.salinity
+    dlnawds = dlnasw_ds(Tc, S)
+
+    # density derivative of refractive index from PMH model
+    DFRI = PMH(nsw)  # PMH model
+
+    # volume scattering at 90 degree due to the density fluctuation
+    beta_df = (
+        np.pi * np.pi / 2 * ((lambda_ * 1e-9) ** (-4)) * Kbz * Tk * IsoComp * DFRI ** 2
+        * (6 + 6 * delta) / (6 - 7 * delta)
+    )
+
+    # volume scattering at 90 degree due to the concentration fluctuation
+    flu_con = S * M0 * dnds ** 2 / density_sw / (-dlnawds) / Na
+    beta_cf = (
+        2 * np.pi * np.pi * ((lambda_ * 1e-9) ** (-4)) * nsw ** 2
+        * (flu_con) * (6 + 6 * delta)/(6 - 7 * delta)
+    )
+
+    # total volume scattering at 90 degree
+    beta90sw = beta_df + beta_cf
+    bsw = 8 * np.pi/3 * beta90sw * (2 + delta) / (1 + delta)
+
+    for i, value in enumerate(rad):  # TODO: Is there a better way to do this?
+        if np.rad2deg(value) >= 124:
+            rad124 = i
+            break
     
-    
-    """
-    pass
+    betasw124 = (
+        beta90sw * (1 + ((np.cos(rad[rad124])) ** 2) * (1 - delta) / (1 + delta))
+    )
+
+    return betasw124, bsw, beta90sw, theta
--- tests/test_water_iops.py
+++ tests/test_water_iops.py
@@ -1,38 +1,93 @@
 import numpy as np
 import pytest
 
-from pigments_from_rrs.water_iops import RInw
+from pigments_from_rrs.water_iops import (
+    RInw,
+    BetaT,
+    rho_sw,
+    dlnasw_ds,
+    PMH,
+    betasw124_ZHH2009,
+)
+
+
+def round_result(actual_result):
+    truncated_actual_result = tuple(
+        [
+            (round(n, ndigits=2) if n > 1 else round(n, ndigits=5))
+            for n in actual_result
+        ]
+    )
+    return truncated_actual_result
 
 
 @pytest.mark.parametrize(
     "kwargs_with_result",
         [
-            (dict(lambda_=400, Tc=10, S=30), (1.35, 0.000197)),
-            (dict(lambda_=650, Tc=18, S=37), (1.33, 0.000182)),
+            (dict(lambda_=400, Tc=10, S=30), (1.35, 0.00020)),
+            (dict(lambda_=650, Tc=18, S=37), (1.34, 0.00018)),
             (
                 dict(lambda_=np.asarray(list(range(400, 701, 50))), Tc=18, S=37),
                 (
-                    np.asarray([1.35, 1.34, 1.34, 1.34, 1.34, 1.33, 1.33]),
-                    np.asarray([0.000193, 0.000189, 0.000187, 0.000185, 0.000183, 0.000182, 0.000180]),
+                    np.asarray([1.35, 1.35, 1.34, 1.34, 1.34, 1.34, 1.34]),
+                    np.asarray([0.00019, 0.00019, 0.00019, 0.00019, 0.00018, 0.00018, 0.00018]),
                 ),
 
             ),
         ]
 )
 def test_RInw(kwargs_with_result):
-    def truncate(actual_result):
-        truncated_actual_result = tuple(
-            [
-                (float(str(n)[:4]) if n > 1 else float(str(n)[:8]))
-                for n in actual_result
-            ]
-        )
-        return truncated_actual_result
-
     kwargs, expected_result = kwargs_with_result
     actual_result = RInw(**kwargs)
     if isinstance(kwargs["lambda_"], int):
-        assert truncate(actual_result) == expected_result
+        assert round_result(actual_result) == expected_result
     else:
         for arr0, arr1 in zip(expected_result, actual_result):
-            assert (arr0 == truncate(arr1)).all()
+            assert (arr0 == round_result(arr1)).all()
+
+
+def test_BetaT():
+    expected_result = 4.313087388409383e-10
+    actual_result = BetaT(Tc=18.0, S=32.0)
+    assert expected_result == actual_result
+
+
+def test_rho_sw():
+    expected_result = 1022.9769302697676
+    actual_result = rho_sw(Tc=18.0, S=32.0)
+    assert expected_result == actual_result
+
+
+def test_dlnasw_ds():
+    expected_result = -0.0005627893517437484
+    actual_result = dlnasw_ds(Tc=18.0, S=32.0)
+    assert expected_result == actual_result
+
+
+def test_PMH():
+    expected_result = np.asarray(
+        [0.90895, 0.87445, 0.87445, 0.87445, 0.87445, 0.84064, 0.84064]
+    )
+    actual_result = PMH(
+        n_wat=np.asarray([1.35, 1.34, 1.34, 1.34, 1.34, 1.33, 1.33])
+    )
+    assert (expected_result == round_result(actual_result)).all()
+
+
+def test_betasw124_ZHH2009():
+    expected_results = (
+        np.asarray([5.2e-04, 3.2e-04, 2.0e-04, 1.4e-04, 0.00009, 0.00007, 0.00005]),
+        np.asarray([0.00668, 0.00403, 0.00258, 0.00173, 0.00120, 0.00086, 0.00063]),
+        np.asarray([4.1e-04, 2.5e-04, 1.6e-04, 1.1e-04, 7e-05, 5e-05, 4e-05]),
+        np.linspace(0.0, 180.0, 18_001),
+    )
+    actual_results = betasw124_ZHH2009(
+        lambda_=np.asarray(list(range(400, 701, 50))),
+        S=37,
+        Tc=18,
+    )
+    for arr0, arr1 in zip(expected_results, actual_results):
+        if len(arr1) != 18_001:
+            assert (arr0 == round_result(arr1)).all()
+        else:  # this is theta!
+            assert (arr0 == arr1).all()
